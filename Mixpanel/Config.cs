4622286af30557e7232de51edb0a9f54e43643c2
Olha Hlushko <ohl@ciklum.com>
2020-08-10 19:15:38 +0300

    remove /?ip=1 from urls

--- Mixpanel/Config.cs
+++ Mixpanel/Config.cs
@@ -3,8 +3,8 @@ namespace mixpanel
     internal static class Config
     {
         // Can be overriden by MixpanelSettings
-        internal static string TrackUrl = "https://api.mixpanel.com/track/?ip=1";
-        internal static string EngageUrl = "https://api.mixpanel.com/engage/?ip=1";
+        internal static string TrackUrl = "https://api.mixpanel.com/track";
+        internal static string EngageUrl = "https://api.mixpanel.com/engage";
         internal static bool ShowDebug = false;
         internal static bool ManualInitialization = false;
         internal static float FlushInterval = 60f;
